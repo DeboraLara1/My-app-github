ba436a465f493e4e8be836e8aaf33efd977801c3
Debora Lara <debora.roldanlara@gmail.com>
2021-02-17 16:16:32 -0200

    atualizações necessarias para a automatização do componente user-info e validações no appContent

--- src/App.js
+++ src/App.js
@@ -4,10 +4,37 @@ import AppContent from './compenents/AppContent';
 class App extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      userinfo: {
+        photo: 'https://avatars.githubusercontent.com/u/59708083?v=4',
+        login: 'deboralara1',
+        username: 'Debora Lara',
+        repos: 12,
+        follwers: 10,
+        following: 10,
+      },
+      repos: [
+        {
+          name: 'Repo',
+          link: '#',
+        },
+      ],
+      starred: [
+        {
+          name: 'Repo',
+          link: '#',
+        },
+      ],
+    };
   }
   render() {
-    return <AppContent />;
+    return (
+      <AppContent
+        userinfo={this.state.userinfo}
+        repos={this.state.repos}
+        starred={this.state.starred}
+      />
+    );
   }
 }
 
