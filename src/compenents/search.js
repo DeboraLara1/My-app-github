4c8716ceee6b37fcdffca96d380798839c91a0d2
Debora Lara <debora.roldanlara@gmail.com>
2021-02-17 14:09:39 -0200

    Separando os componentes

---
+++ src/compenents/search.js
@@ -0,0 +1,9 @@
+'use strict';
+import React from 'react';
+
+const Search = () => (
+  <div className="shearch">
+    <input type="shearch" placeholder="Digite o nome do usuário no GitHub" />
+  </div>
+);
+export default Search;
---
+++ src/compenents/user-info.js
@@ -0,0 +1,16 @@
+'use strict';
+import React from 'react';
+const UserInfo = () => (
+  <div className="user-info">
+    <img src="https://avatars.githubusercontent.com/u/59708083?v=4" alt="" />
+    <h1>
+      <a href="https://github.com/deboralara1"> Debora Lara</a>
+    </h1>
+    <ul className="repos-info">
+      <li>Repositório: 111</li>
+      <li>Seguidores:</li>
+      <li>Seguindo</li>
+    </ul>
+  </div>
+);
+export default UserInfo;
